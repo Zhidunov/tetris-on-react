cfea43645bf9e1a3a97ac1a71ea5751e19207cec
Nikita Zhidunov <zhidunov@gmail.com>
2020-05-28 01:36:17 +0300

    Added full rows clear function

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,22 @@ import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
   let [stage, setStage] = useState(createStage());
+  let [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = (newStage) =>
+      newStage.reduce((accumulator, row) => {
+        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+          setRowsCleared((prev) => prev + 1);
+          accumulator.unshift(new Array(newStage[0].length).fill([0, "clear"]));
+          return accumulator;
+        }
+        accumulator.push(row);
+        return accumulator;
+      }, []);
+
     const updateStage = (prevStage) => {
       const newStage = prevStage.map((row) =>
         row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
@@ -21,16 +35,16 @@ export const useStage = (player, resetPlayer) => {
         });
       });
 
-      if(player.collided){
+      if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
 
       return newStage;
     };
 
-
     setStage((prev) => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
