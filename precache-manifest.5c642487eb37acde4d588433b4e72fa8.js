d1abfeb6615d858592dfbe7dad5d092b14c91e40
Nikita Zhidunov <zhidunov@gmail.com>
2020-05-28 02:34:13 +0300

    Updates

---
+++ precache-manifest.5c642487eb37acde4d588433b4e72fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4b28cd03a9106c82c52db844f086ea",
+    "url": "/tetris-on-react/index.html"
+  },
+  {
+    "revision": "efa47f370503e2383f6a",
+    "url": "/tetris-on-react/static/css/main.3c4def4f.chunk.css"
+  },
+  {
+    "revision": "4902c0d0933aa60f4935",
+    "url": "/tetris-on-react/static/js/2.ce85fe83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris-on-react/static/js/2.ce85fe83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa47f370503e2383f6a",
+    "url": "/tetris-on-react/static/js/main.3e3d78d0.chunk.js"
+  },
+  {
+    "revision": "742c3c5f26d03aaa7dc8",
+    "url": "/tetris-on-react/static/js/runtime-main.113b25cb.js"
+  }
+]);
